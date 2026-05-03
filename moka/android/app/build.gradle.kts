bedecdb8c811c5d51a4c6cfc5601082fafb89acf
Mr. Bright <brightakpla2@gmail.com>
2026-05-03 21:06:19 +0000

    the fix 1

--- moka/android/app/build.gradle.kts
+++ moka/android/app/build.gradle.kts
@@ -11,8 +11,13 @@ android {
     ndkVersion = "28.2.13676358"
 
     compileOptions {
+        coreLibraryDesugaringEnabled true 
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    dependencies {
+        coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.0.4'  
     }
 
     kotlinOptions {
